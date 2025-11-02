e21b1436cfe53454a7b9ff103c93bd3b59eb8357
Fabrem <fjbremermann@gmail.com>
2025-11-01 23:28:08 -0300

    Pantalla basica de registro de usuario

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,4 @@
 export 'package:smart_route_app/presentation/screens/home/home_screen.dart';
 export 'package:smart_route_app/presentation/screens/login/login_screen.dart';
-export 'package:smart_route_app/presentation/screens/login/succesful_login_screen.dart';
+export 'package:smart_route_app/presentation/screens/signup/succesful_signup_screen.dart';
 export 'package:smart_route_app/presentation/screens/signup/signup_screen.dart';
---
+++ lib/presentation/screens/signup/succesful_signup_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SuccesfulSignupScreen extends StatelessWidget {
+  static const name = 'succesfull-signup-screen';
+
+  const SuccesfulSignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
