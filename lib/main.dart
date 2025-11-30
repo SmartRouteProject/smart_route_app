8790e5267b5757b0fba8b3d26e0f4c16ce15df7e
Fabrem <fjbremermann@gmail.com>
2025-11-29 22:35:41 -0300

    Configuracion de variables de entorno

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
 
+import 'package:smart_route_app/config/constants/env.dart';
 import 'package:smart_route_app/config/router/app_router.dart';
 import 'package:smart_route_app/config/theme/app_theme.dart';
 
@@ -9,6 +10,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Geolocator.requestPermission();
+
+  await Env.init();
 
   runApp(const ProviderScope(child: MyApp()));
 }
