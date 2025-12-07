62faff5becd6ab5c187f5c651aba49ce396ed46d
Fabrem <fjbremermann@gmail.com>
2025-12-07 16:26:09 -0300

    Estado inicial para el formulario del perfil

--- lib/presentation/providers/profile_form_provider.dart
+++ lib/presentation/providers/profile_form_provider.dart
@@ -1,22 +1,60 @@
 import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:formz/formz.dart';
 import 'package:image_picker/image_picker.dart';
 
-final profileFormProvider = StateNotifierProvider<ProfileFormNotifier, File?>(
-  (ref) => ProfileFormNotifier(),
-);
+import 'package:smart_route_app/infrastructure/inputs/inputs.dart';
 
-class ProfileFormNotifier extends StateNotifier<File?> {
-  ProfileFormNotifier() : super(null);
+final profileFormProvider =
+    StateNotifierProvider<ProfileFormNotifier, ProfileFormState>(
+      (ref) => ProfileFormNotifier(),
+    );
+
+class ProfileFormNotifier extends StateNotifier<ProfileFormState> {
+  ProfileFormNotifier() : super(ProfileFormState());
 
   final ImagePicker _picker = ImagePicker();
+
+  Future<bool> onFormSubmit() async {
+    try {
+      _touchEveryField();
+
+      if (!state.isValid) return false;
+
+      state = state.copyWith(isPosting: true);
+
+      state = state.copyWith(isPosting: false);
+
+      return true;
+    } catch (err) {
+      state = state.copyWith(isPosting: false);
+      return false;
+    }
+  }
+
+  onUserNameChange(String value) {
+    final newUserName = UserName.dirty(value);
+    state = state.copyWith(
+      userName: newUserName,
+      isValid: Formz.validate([newUserName, state.userLastName]),
+    );
+  }
+
+  onUserLastnameChange(String value) {
+    final newUserLastname = UserName.dirty(value);
+    state = state.copyWith(
+      userLastName: newUserLastname,
+      isValid: Formz.validate([state.userName, newUserLastname]),
+    );
+  }
 
   Future<void> pickFromGallery() async {
     final picked = await _picker.pickImage(
       source: ImageSource.gallery,
       imageQuality: 70,
     );
-    if (picked != null) state = File(picked.path);
+    if (picked != null)
+      state = state.copyWith(profilePicture: File(picked.path));
   }
 
   Future<void> pickFromCamera() async {
@@ -24,10 +62,58 @@ class ProfileFormNotifier extends StateNotifier<File?> {
       source: ImageSource.camera,
       imageQuality: 70,
     );
-    if (picked != null) state = File(picked.path);
+    if (picked != null)
+      state = state.copyWith(profilePicture: File(picked.path));
   }
 
   void clearImage() {
-    state = null;
+    state = state = state.copyWith(profilePicture: null);
+    ;
   }
+
+  _touchEveryField() {
+    final userName = UserName.dirty(state.userName.value);
+    final userLastName = UserName.dirty(state.userLastName.value);
+
+    state = state.copyWith(
+      isFormPosted: true,
+      userName: userName,
+      userLastName: userLastName,
+      isValid: Formz.validate([userName, userLastName]),
+    );
+  }
+}
+
+class ProfileFormState {
+  final bool isPosting;
+  final bool isFormPosted;
+  final bool isValid;
+  final UserName userName;
+  final UserName userLastName;
+  final File? profilePicture;
+
+  ProfileFormState({
+    this.isPosting = false,
+    this.isFormPosted = false,
+    this.isValid = false,
+    this.userName = const UserName.pure(),
+    this.userLastName = const UserName.pure(),
+    this.profilePicture,
+  });
+
+  ProfileFormState copyWith({
+    bool? isPosting,
+    bool? isFormPosted,
+    bool? isValid,
+    UserName? userName,
+    UserName? userLastName,
+    File? profilePicture,
+  }) => ProfileFormState(
+    isPosting: isPosting ?? this.isPosting,
+    isFormPosted: isFormPosted ?? this.isFormPosted,
+    isValid: isValid ?? this.isValid,
+    userName: userName ?? this.userName,
+    userLastName: userLastName ?? this.userLastName,
+    profilePicture: profilePicture ?? this.profilePicture,
+  );
 }
