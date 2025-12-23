2e79e49333dd32a9a3877151df2f3140da5a1e4a
Facundo Bremermann <fbremermann@ica.com.uy>
2025-12-23 19:58:19 -0300

    Metodos para enums para obtener su label y para pasar de string a enum

--- lib/domain/enums/package_weight_type.dart
+++ lib/domain/enums/package_weight_type.dart
@@ -1,1 +1,24 @@
-enum PackageWeightType { under25, over25 }
+enum PackageWeightType {
+  under25,
+  over25;
+
+  static PackageWeightType fromString(String value) {
+    return PackageWeightType.values.firstWhere(
+      (e) => e.name.toLowerCase() == value.toLowerCase(),
+      orElse: () => PackageWeightType.under25,
+    );
+  }
+}
+
+extension PackageWeightTypeExt on PackageWeightType {
+  String getStringValue() => name;
+
+  String get label {
+    switch (this) {
+      case PackageWeightType.under25:
+        return 'Menos de 25kg';
+      case PackageWeightType.over25:
+        return 'Mas de 25kg';
+    }
+  }
+}
